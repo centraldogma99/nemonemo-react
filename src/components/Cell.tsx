946a9bf5bb29c225e86914df92b6ff200abc2e71
최준영 <jychoi@jobis.co>
2024-11-09 23:06:05 +0900

    add subpath import

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,5 +1,5 @@
+import { CellStatus } from "#constants/Cell.ts"
 import type { ComponentProps } from "react"
-import { CellStatus } from "../constants/Cell"
 
 interface CellProps extends ComponentProps<"td"> {
   status: CellStatus
@@ -8,7 +8,7 @@ interface CellProps extends ComponentProps<"td"> {
 export const Cell = ({ status, ...tdProps }: CellProps) => {
   return (
     <td
-      className={`${status === CellStatus.Filled ? "bg-black" : ""} select-none ${tdProps.className}`}
+      className={`select-none ${status === CellStatus.Filled ? "bg-black" : ""} ${tdProps.className}`}
       {...tdProps}
     >
       {status === CellStatus.X && "X"}
